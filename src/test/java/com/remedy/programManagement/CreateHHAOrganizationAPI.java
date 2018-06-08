9cdb2db7f8f6ef5f07920076026efbc440c9dccd
Anubhav Anand <aanand@remedypartners.com>
2018-06-08 18:34:39 +0530

    Implementing Create HHA API calls

---
+++ src/test/java/com/remedy/programManagement/CreateHHAOrganizationAPI.java
@@ -0,0 +1,13 @@
+package com.remedy.programManagement;
+
+import org.openqa.selenium.WebDriver;
+
+import com.remedy.baseClass.BaseClass;
+
+public class CreateHHAOrganizationAPI extends BaseClass{
+
+	public CreateHHAOrganizationAPI(WebDriver driver) {
+		super(driver);
+	}
+
+}
---
+++ src/test/java/stepDefination/programManagement/CreateHHAOrganizationAPIStepDef.java
@@ -0,0 +1,9 @@
+package stepDefination.programManagement;
+
+import com.remedy.programManagement.CreateHHAOrganizationAPI;
+import com.remedy.resources.DriverScript;
+
+public class CreateHHAOrganizationAPIStepDef extends DriverScript{
+
+	CreateHHAOrganizationAPI createHHAAPI = new CreateHHAOrganizationAPI(driver);
+}
