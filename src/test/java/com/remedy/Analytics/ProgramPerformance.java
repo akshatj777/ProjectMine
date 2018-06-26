8dfd6bd63513d4dd5db9c25194d6dfd67a48875e
Sandeep Singh <ssingh@remedypartners.com>
2018-06-26 18:05:44 +0530

    Automated greyscale conversion for image

--- src/test/java/com/remedy/Analytics/ProgramPerformance.java
+++ src/test/java/com/remedy/Analytics/ProgramPerformance.java
@@ -326,6 +326,7 @@ public class ProgramPerformance extends BaseClass{
 					screenshotLocation.delete();
 				}
 				FileUtils.copyFile(screenshot, screenshotLocation);
+//				greyscale();
 //				TextFromImage();
 				TessBaseAPI instance=new TessBaseAPI();
 				 instance.Init(System.getProperty("user.dir")+"\\src\\test\\Imports\\TestData\\tessdata", "eng");
@@ -400,4 +401,49 @@ public class ProgramPerformance extends BaseClass{
 
 		return ret;
 	}
+	
+	 public void greyscale()throws IOException{
+		    BufferedImage img = null;
+		    File f = null;
+
+		    //read image
+		    try{
+		      f = new File(System.getProperty("user.dir")+"\\src\\test\\Imports\\Image2.png");
+		      img = ImageIO.read(f);
+		    }catch(IOException e){
+		      System.out.println(e);
+		    }
+
+		    //get image width and height
+		    int width = img.getWidth();
+		    int height = img.getHeight();
+
+		    //convert to grayscale
+		    for(int y = 0; y < height; y++){
+		      for(int x = 0; x < width; x++){
+		        int p = img.getRGB(x,y);
+
+		        int a = (p>>24)&0xff;
+		        int r = (p>>16)&0xff;
+		        int g = (p>>8)&0xff;
+		        int b = p&0xff;
+
+		        //calculate average
+		        int avg = (r+g+b)/3;
+
+		        //replace RGB value with avg
+		        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+
+		        img.setRGB(x, y, p);
+		      }
+		    }
+
+		    //write image
+		    try{
+		      f = new File(System.getProperty("user.dir")+"\\src\\test\\Imports\\Image3.png");
+		      ImageIO.write(img, "jpg", f);
+		    }catch(IOException e){
+		      System.out.println(e);
+		    }
+		  }//main() ends here
 }
