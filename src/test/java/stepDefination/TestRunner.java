15b4c1a851cf626bb59897c9423cb034555cc924
Priyal Dubey <priyald@citiustech.com>
2018-08-08 15:24:11 +0530

    corrected DP edit FF

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -36,7 +36,7 @@ import stepDefination.Hooks.*;
                 "com.cucumber.listener.ExtentCucumberFormatter:target/vimalSelvam-cucumberReport/report.html"},
 
 
-        features = { "src/test/Features/UA2/PTA/EditNew.feature" }
+        features = { "src/test/Features/UA2/SuperAdmin/EditUser.feature" }
 
 
         //glue = { "./src/test/java/stepDefination" },
