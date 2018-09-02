bfea91f758f4e0bc63be284e60eff234556fbf37
Deepak Bithar <dbithar@remedypartners.com>
2018-09-02 15:09:24 +0530

    Fixed failures in the FF

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -36,7 +36,7 @@ import stepDefination.Hooks.*;
                 "com.cucumber.listener.ExtentCucumberFormatter:target/vimalSelvam-cucumberReport/report.html"},
 
 
-        features = { "src/test/Features/UA2/SuperAdmin/CreateUserFullApplicationAccess.feature" }
+        features = { "src/test/Features/" }
 
 
         //glue = { "./src/test/java/stepDefination" },
