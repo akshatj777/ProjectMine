0072fb9f16439aaec1bce9f5523d95bb60ad6631
Deepak Bithar <dbithar@remedypartners.com>
2018-09-10 14:08:30 +0530

    Fixed failures

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -36,7 +36,7 @@ import stepDefination.Hooks.*;
                 "com.cucumber.listener.ExtentCucumberFormatter:target/vimalSelvam-cucumberReport/report.html"},
 
 
-        features = { "src/test/Features/UA2/test/" }
+        features = { "src/test/Features/" }
 
 
         //glue = { "./src/test/java/stepDefination" },
