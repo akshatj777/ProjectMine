9e365e9d0d6caaf14682bf8b7e543ba5a10cae62
Vivek Ranjan <vranjan@remedypartners.com>
2018-10-18 18:25:11 +0530

    REP-4935 Started automating drill through for performance overview

---
+++ src/test/java/stepDefination/Analytics/NSOCDashboardSteps.java
@@ -0,0 +1,10 @@
+package stepDefination.Analytics;
+
+import com.remedy.Analytics.NSOCDashboard;
+import com.remedy.resources.DriverScript;
+
+public class NSOCDashboardSteps extends DriverScript {
+	
+	NSOCDashboard nsoc = new NSOCDashboard(driver);
+
+}
