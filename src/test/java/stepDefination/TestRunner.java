3bb8150c48a1bb67abb86f2809ee520006169da8
Vivek Ranjan <vranjan@remedypartners.com>
2018-10-29 10:17:14 +0530

    Worked on fixing failures during execution of row level security

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -38,7 +38,7 @@ import stepDefination.Hooks.*;
         monochrome = true,
         //glue = { "./src/test/java/stepDefination" },
                                    
-        tags = {"@Claims"}
+        tags = {"@RowLevelSecurity"}
 )
 public class TestRunner {
 
