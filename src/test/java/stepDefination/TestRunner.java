b95398a0dd6c5c4938033b115a278e4164e4bef0
Vivek Ranjan <vranjan@remedypartners.com>
2018-11-06 15:53:28 +0530

    REP-6340 Worked on fixing failures

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -37,7 +37,7 @@ import stepDefination.Hooks.*;
         features = { "src/test/Features/" },
         monochrome = true,
         //glue = { "./src/test/java/stepDefination" },
-        tags = {"@tag2"}
+        tags = {"@Claims"}
 )
 public class TestRunner {
 
